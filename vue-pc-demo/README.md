43b5e493ea647cf8803db376794513672941bc4d
v_renjuyuan <v_renjuyuan@tencent.com>
2021-04-16 18:01:28 +0800

    init

--- vue-pc-demo/README.md
+++ vue-pc-demo/README.md
@@ -1,5 +1,55 @@
 # vue-pc-demo
 
+## 初始化项目准备工作
+### pc端初始化代码
+- 配置：less + (ESLint + Prettier ) + (Lint on save) + (In dedicated config files)
+
+### vue.config.js 配置
+- TODO
+
+### router
+- TODO
+
+### vuex
+- TODO
+
+### css-loder
+- TODO
+- postcss.config.js (传给postcss-loader的相关配置)
+
+### ESLINT
+- TODO
+- eslint 配置
+
+### git相关配置
+-TODO
+
+### 环境变量配置
+- .env.loc ---本地环境
+- .env.prd ---正式环境
+- .env.uat ---测试环境
+
+### 编辑器规范
+- TODO
+- editorconfig相关配置
+- browserslistrc相关配置
+
+### axios配置
+- loading  配置
+- TODO
+
+### 字典项配置
+- TODO
+
+### 公用方法
+- utils 
+- TODO
+
+### mock使用
+- TODO
+
+### vue 多页面
+- TODO
 ## Project setup
 ```
 npm install
@@ -24,8 +74,5 @@ npm run test
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
 
 - 接下来，就开始你的快乐编程之旅把!
---
+++ vue-pc-demo/vue.config.js
@@ -0,0 +1,64 @@
+// vue.config.js
+// webpack 压缩插件
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  assetsDir: "static",
+  publicPath: "/",
+  outputDir: "dist",
+  configureWebpack: config => {
+    // 生产环境配置
+    if (process.env.NODE_ENV === 'production') {
+      // Gzip 压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          // 正在匹配需要压缩的文件后缀
+          test: /.(js|css|svg|woff|ttf|json|html)$/,
+          // 大于10kb的会压缩
+          threshold: 10240
+        })
+      )
+    } else {
+      // 开发环境配置
+    }
+  },
+  css: {
+    extract: false,
+    sourceMap: false,
+    //向css相关的loader传递选项，
+    loaderOptions: {
+      //支持的loader:css-loader,postcss-loader,sass-loader,less-loader,stylus-loader
+      css: {
+        //这里的选项会传递给 css-loader
+      },
+      postcss: {
+        //这里的选项会传递给 postcss-loader
+      }
+    }
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, "src/assets/css/mixin.less")] // 引入全局样式变量
+    }
+  },
+  productionSourceMap: false,
+  // 开发环境Node配置
+  devServer: {
+    open: true, //配置自动启动浏览器
+    hotOnly: true, //热更新
+    port: 8080,
+    host: '0.0.0.0',
+    // 代理配置
+    proxy: {
+      '^/api': {
+        headers: {
+          'aaa': '111'
+        },
+        // 目标 API 地址
+        target: 'http://0.0.0.0:8000' 
+      }
+    }
+  }
+}
