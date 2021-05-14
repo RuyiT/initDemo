89fa108d99d436ff75fdf8a9b3e08d7a9bba20c8
v_renjuyuan <v_renjuyuan@tencent.com>
2021-05-14 16:14:06 +0800

    css案例

---
+++ vue-pc-demo/src/views/cssviews/hover.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="box">
+    <ul class="hover-tips">
+      <li data-name="姨妈红"></li>
+      <li data-name="基佬紫"></li>
+      <li data-name="箩底橙"></li>
+      <li data-name="姣婆蓝"></li>
+      <li data-name="大粪青"></li>
+      <li data-name="原谅绿"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.box {
+  @color-list: {
+    a: #f66;
+    b: #66f;
+    c: #f90;
+    d: #09f;
+    e: #9c3;
+    f: #3c9;
+  };
+  .hover-tips {
+    display: flex;
+    justify-content: space-between;
+    width: 200px;
+    padding: 0;
+    li {
+      list-style: none;
+      position: relative;
+      padding: 2px;
+      border: 2px solid transparent;
+      border-radius: 100%;
+      width: 24px;
+      height: 24px;
+      background-clip: content-box;
+      cursor: pointer;
+      transition: all 300ms;
+      &::before,
+      &::after {
+        position: absolute;
+        left: 50%;
+        bottom: 100%;
+        opacity: 0;
+        transform: translate3d(0, -30px, 0);
+        transition: all 300ms;
+      }
+      &::before {
+        margin: 0 0 12px -35px;
+        border-radius: 5px;
+        width: 70px;
+        height: 30px;
+        background-color: rgba(#000, 0.5);
+        line-height: 30px;
+        text-align: center;
+        color: #fff;
+        content: attr(data-name);
+      }
+      &::after {
+        margin-left: -6px;
+        border: 6px solid transparent;
+        border-top-color: rgba(#000, 0.5);
+        width: 0;
+        height: 0;
+        content: '';
+      }
+      /**
+      scss 的each使用方法
+      */
+      // @each $color in --color-list {
+      //     $index: index(--color-list, $color);
+      //     &:nth-child(#{$index}) {
+      //         background-color: $color;
+      //         &:hover {
+      //             border-color: $color;
+      //         }
+      //     }
+      // }
+
+      /**
+      less 的each使用方法
+      https://www.jianshu.com/p/c7c210bd25e8
+      */
+      each(@color-list, {
+        &:nth-child(@{index}) {
+            background-color: @value;
+            &:hover {
+              border-color: @value;
+            }
+        }
+      })
+        &:hover {
+        &::before,
+        &::after {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+        }
+      }
+    }
+  }
+}
+</style>
--- vue-pc-demo/src/views/home.vue
+++ vue-pc-demo/src/views/home.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <div @click="handleClick">请求数据</div>
-    <div @click="handleClick2">请求数据2</div>
-    <iframe
+  <div class="home">
+    <div>
+      <div @click="handleClick">请求数据</div>
+      <div @click="handleClick2">请求数据2</div>
+      <hover></hover>
+      <div class="bg">
+        <div class="bg-1"></div>
+        <div class="bg-2"></div>
+      </div>
+      <i-login></i-login>
+      <i-rview></i-rview>
+      <i-srcoll></i-srcoll>
+      <router-view></router-view>
+    </div>
+
+    <!-- <iframe
       src="https://www.baidu.com"
       style="
-        top: 500px;
-        left: 0;
+        top: 0;
+        left: 800px;
         bottom: 0;
         width: 100%;
         height: 100%;
@@ -14,17 +26,26 @@
         border: none;
       "
     >
-    </iframe>
-    <router-view></router-view>
+    </iframe> -->
   </div>
 </template>
 
 <script>
 import * as adminService from '@/services/home.services'
+import hover from './cssviews/hover.vue'
+import iLogin from './cssviews/login'
+import iRview from './cssviews/rview'
+import iSrcoll from './cssviews/srcoll'
 export default {
   name: 'home',
   data() {
     return {}
+  },
+  components: {
+    hover,
+    iLogin,
+    iRview,
+    iSrcoll,
   },
   methods: {
     handleClick() {
@@ -43,4 +64,35 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+@bg-1: 'https://static.yangzw.vip/codepen/ab-1.jpg';
+@bg-2: 'https://static.yangzw.vip/codepen/ab-2.jpg';
+@bg-3: 'https://static.yangzw.vip/codepen/mountain.jpg';
+@bg-4: 'https://static.yangzw.vip/codepen/logo.svg';
+.home {
+  display: flex;
+}
+.bg {
+  width: 100%;
+  height: 300px;
+  display: flex;
+  .bg-1 {
+    width: 300px;
+    height: 200px;
+    background-color: #3c9;
+    background-image: url(@bg-1), url(@bg-2);
+    background-repeat: no-repeat, no-repeat;
+    background-position: left, right;
+    background-size: auto 200px, auto 200px;
+  }
+  .bg-2 {
+    margin-left: 20px;
+    width: 300px;
+    height: 200px;
+    background-image: url(@bg-4), url(@bg-3);
+    background-repeat: repeat, no-repeat;
+    background-position: left, center;
+    background-size: auto 80px, auto 200px;
+  }
+}
+</style>
